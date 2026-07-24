62b4548530fe53e078e53a1f1515b06f3b96aa03
amndaone <onea@oregonstate.edu>
2026-07-24 00:02:34 -0700

    add revenue column and extract time features

---
+++ sql/data_features.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS online_retail_features;
+
+SELECT
+    *,
+    quantity * unit_price AS revenue,
+    EXTRACT(QUARTER FROM invoice_date) AS invoice_quarter,
+    EXTRACT(YEAR FROM invoice_date) AS invoice_year,
+    EXTRACT(MONTH FROM invoice_date) AS invoice_month,
+    TRIM(TO_CHAR(invoice_date, 'Month')) AS month_name,
+    EXTRACT(DOW FROM invoice_date) AS day_of_week,
+    TRIM(TO_CHAR(invoice_date, 'Day')) AS day_name,
+    EXTRACT(HOUR FROM invoice_date) AS invoice_hour
+FROM online_retail_clean;
